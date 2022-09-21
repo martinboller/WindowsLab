c1f307718e69f97a70d391951c66d9a871a65ebc
martinboller <martin@bollers.dk>
2022-09-21 20:18:22 +0200

    Sysprep

--- Packer/scripts/sysprep.bat
+++ Packer/scripts/sysprep.bat
@@ -1,4 +1,4 @@
-net stop tiledatamodelsvc
-echo "I am shutting down"
+REM net stop tiledatamodelsvc
+echo "Running Sysprep and shutting down"
 c:\windows\system32\sysprep\sysprep.exe /generalize /mode:vm /oobe /unattend:a:\unattend.xml
 shutdown /s
